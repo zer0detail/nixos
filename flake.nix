2170da05132e7c175067f7ec6f01187a91912ad4
zerodetail <zerodetail@gmail.com>
2023-09-17 19:31:36 +1000

    lost

--- flake.nix
+++ flake.nix
@@ -14,7 +14,8 @@
   };
   inputs = {
     # official NixOS Package source
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
