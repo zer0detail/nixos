1a7692e2150a4bca3eab3276394c05b0f1d6cf40
zerodetail <zerodetail@gmail.com>
2023-09-18 15:09:48 +1000

    remove helix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, helix, ... }:
+{ config, pkgs, ... }:
 
 {
   imports =
@@ -121,7 +121,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    helix 
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
