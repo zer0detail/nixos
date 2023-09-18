bfb094f38fdc636f0e290a145127262ab697390b
zerodetail <zerodetail@gmail.com>
2023-09-18 14:37:05 +1000

    adding default.nix files

---
+++ overlays/default.nix
@@ -0,0 +1,13 @@
+{inputs, ... }: {
+  additions = final: _prev: import ../pkgs {pkgs final;};
+
+  modifications = final: prev: {
+
+  }
+
+  unstable-packages = final: _prev: {
+    unstable = import inputs.nixpkgs-unstable {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  }
