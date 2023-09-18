c248a53d5bd3078594fee4ba9bf38895c1a13755
zerodetail <zerodetail@gmail.com>
2023-09-18 16:48:49 +1000

    kitty config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,28 +2,31 @@
 
 {
   imports = [
-    ./nvim
+   ./kitty
   ];
   home.username = "zero";
   home.homeDirectory = "/home/zero";
 
+  fonts.fontconfig.enable = true;
   # Basic git config
   programs.git = {
     enable = true;
     userName = "zerodetail";
     userEmail = "zerodetail@gmail.com";
   };
- #programs.neovim = {
-  #  enable = true;
+ programs.neovim = {
+    enable = true;
 
- #   viAlias = true;
- #   vimAlias = true;
- #   vimdiffAlias = true;
- #}; 
+    viAlias = true;
+     vimAlias = true;
+     vimdiffAlias = true;
+  }; 
+  xdg.configFile.nvim.source = ./nvim;
 
   # packages that should be installed to the user profile.
   home.packages = with pkgs; [
     neofetch  
+    (nerdfonts.override { fonts = [ "FiraCode"]; })
   ];
 
   home.stateVersion = "23.05";
---
+++ home-manager/kitty/default.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_family = "FiraCode";
+      font_size = "10.5";
+      adjust_line_height = "0";
+      adjust_column_width = "0";
+      disable_ligatures = "never";
+      cursor_shape = "underline";
+      scrollback_lines = "9000";
+      mouse_hide_wait = "3.0";
+      url_style = "single";
+      open_url_with = "default";
+      copy_on_select = "yes";
+      strip_trailing_spaces = "smart";
+      focus_follows_mouse = "yes";
+      repaint_delay = "10";
+      input_delay = "1";
+      sync_to_monitor = "no";
+      remember_window_size = "yes";
+      initial_window_width = "1920";
+      initial_window_hiehgt = "1080";
+      enabled_layouts = "tall:bias=70,full_size=1";
+      window_border_width = "1.0";
+      draw_minimal_borders = "yes";
+      window_margin_width = "0.0";
+      active_border_color = "#00ff00";
+      inactive_border_color = "#cccccc";
+      bell_border_color = "#ff5a00";
+      inactive_text_alpha = "1.0";
+      hide_window_decorations = "yes";
+      resize_debounce_time = "0.1";
+      resize_draw_strategy = "static";
+      kitty_mod = "ctrl+shift";
+      term = "xterm-kitty";
+      clipboard_control = "write-clipboard write-primary";
+    };
+  };
+}
